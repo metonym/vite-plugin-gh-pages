e1664f671da73d149d778987840cf4fca96f77af
Eric Liu <ericyl.us@gmail.com>
2022-02-06 17:10:01 -0800

    test: add example [ci skip]

---
+++ example/src/index.ts
@@ -0,0 +1,11 @@
+const App = {
+  init: (text: string) => {
+    const body = document.querySelector("body");
+    const h1 = document.createElement("h1");
+
+    h1.innerText = text;
+    body.prepend(h1);
+  },
+};
+
+export default App;
---
+++ example/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import { ghPages } from "vite-plugin-gh-pages";
+
+export default defineConfig({
+  base: "/vite-plugin-gh-pages/",
+  plugins: [ghPages()],
+});
