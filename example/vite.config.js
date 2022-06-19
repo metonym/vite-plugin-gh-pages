8ab367e31ad91594cffbb66b56a13eecc4c2b2db
metonym <ericyl.us@gmail.com>
2022-06-19 14:57:56 -0700

    feat: infer `config.base` from `package.json#name`, print `outDir` and branch in `onPublish` callback (#3)
    * fix: invoke plugin after build plugins
    
    * feat: infer config base from package.json name
    
    * fix: callbacks should override default messages
    
    * feat: print the outDir and branch name in the default onPublish callback

--- example/vite.config.js
+++ example/vite.config.js
@@ -2,12 +2,5 @@ import { defineConfig } from "vite";
 import { ghPages } from "vite-plugin-gh-pages";
 
 export default defineConfig({
-  base: "/vite-plugin-gh-pages/",
-  plugins: [
-    ghPages({
-      onPublish: () => {
-        console.log("🎉");
-      },
-    }),
-  ],
+  plugins: [ghPages()],
 });
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,65 @@
-import { writeFileSync } from "fs";
+import { existsSync, readFileSync, writeFileSync } from "fs";
 import { join } from "path";
 import gp from "gh-pages";
 import type { Plugin } from "vite";
 import type { PublishOptions, publish } from "gh-pages";
 
 interface GhPagesOptions extends PublishOptions {
-  onPublish?: () => void;
+  onPublish?: (publishOptions: PublishOptions & { outDir: string }) => void;
   onError?: Parameters<typeof publish>[2];
 }
+
+const getPackageName = (): undefined | string => {
+  const pkg_path = join(process.cwd(), "package.json");
+
+  if (existsSync(pkg_path)) {
+    const pkg = JSON.parse(readFileSync(pkg_path, "utf-8"));
+    return pkg?.name ?? undefined;
+  } else {
+    return undefined;
+  }
+};
 
 export const ghPages = (options?: GhPagesOptions): Plugin => {
   let outDir = "";
 
+  const onError: GhPagesOptions["onError"] =
+    options?.onError ??
+    ((error) => {
+      console.log(error);
+    });
+
+  const onPublish: GhPagesOptions["onPublish"] =
+    options?.onPublish ??
+    (({ outDir, branch }) => {
+      console.log(`🎉 Published \`${outDir}\` to branch \`${branch}\`.`);
+    });
+
   return {
     name: "vite:gh-pages",
     apply: "build",
+    enforce: "post",
+    config(config) {
+      if (config.base === undefined) {
+        config.base = "/" + getPackageName() + "/";
+      }
+    },
     configResolved(resolvedConfig) {
       outDir = resolvedConfig.build.outDir;
     },
     closeBundle() {
       writeFileSync(join(outDir, ".nojekyll"), "");
-      gp.publish(
-        outDir,
-        {
-          dotfiles: true,
-          ...options,
-        },
-        (error) => {
-          if (error) {
-            options?.onError?.(error);
-            console.log(error);
-            return;
-          }
-          options?.onPublish?.();
-          console.log("Published.");
-        }
-      );
+
+      const gpOptions = {
+        dotfiles: true,
+        branch: "gh-pages",
+        ...options,
+      };
+
+      gp.publish(outDir, gpOptions, (error) => {
+        if (error) return onError(error);
+        onPublish({ ...gpOptions, outDir });
+      });
     },
   };
 };
