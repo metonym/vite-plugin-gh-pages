eea6f2fbe375ff7bcdfc7515b02636c6ef9936a4
metonym <ericyl.us@gmail.com>
2022-06-19 10:16:14 -0700

    refactor: move plugin to `src/index.ts` (#1)
    * chore(deps-dev): bump deps
    
    * refactor: move plugin to src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-export { pluginGhPages as ghPages } from "./plugin-gh-pages";
+import { writeFileSync } from "fs";
+import { join } from "path";
+import gp from "gh-pages";
+import type { Plugin } from "vite";
+import type { PublishOptions } from "gh-pages";
+
+export const ghPages = (options?: PublishOptions): Plugin => {
+  let outDir = "";
+
+  return {
+    name: "vite:gh-pages",
+    apply: "build",
+    configResolved(resolvedConfig) {
+      outDir = resolvedConfig.build.outDir;
+    },
+    closeBundle() {
+      writeFileSync(join(outDir, ".nojekyll"), "");
+      gp.publish(
+        outDir,
+        {
+          dotfiles: true,
+          ...options,
+        },
+        (error) => {
+          if (error) return console.log(error);
+          console.log("Published.");
+        }
+      );
+    },
+  };
+};
